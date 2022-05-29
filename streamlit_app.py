7fbaa501d3fd121f6a67dd8d8a69bdebfd7333c9
RahulR <65364808+thegitr@users.noreply.github.com>
2022-05-29 20:29:38 +0530

    Update streamlit_app.py
    corrected typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text("🥣 Puri")
 streamlit.header("🍒🍒🍒 Make your own fruit smoothie 🍒🍒🍒")
 
 import pandas
-my_frit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
